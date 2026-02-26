7d05513f2fd8ffb32b777c6c05e26307d5dbf256
Skuyrt <imminnd@gmail.com>
2026-02-26 16:33:31 +0100

    2entrega

---
+++ src/1.acabados/niza/datos-piedras.js
@@ -0,0 +1,19 @@
+const catálogoPiedras = {
+    "alcaniz": {
+        titulo: "ALCAÑIZ",
+        subtitulo: "Arenisca de Cantabria",
+        descripcion: "La Arenisca Alcañiz es la esencia misma de la calidez natural. Su característica tonalidad amarilla, de grano fino a medio...",
+        acabados: "Corte sierra, abujardado, trinchante y labrado.",
+        formatos: "Tabla, Aplacados, adoquines, bordillos, piezas a medida.",
+        imagenHero: "../../img/Arenisca.jpg"
+    },
+    "floresta": {
+        titulo: "FLORESTA",
+        subtitulo: "Elegancia Grisácea",
+        descripcion: "La piedra Floresta destaca por su resistencia extrema y sus tonos sobrios, ideal para pavimentos de alto tránsito...",
+        acabados: "Pulido, apomazado y flameado.",
+        formatos: "Grandes placas y adoquines rústicos.",
+        imagenHero: "../../img/floresta_hero.jpg"
+    }
+    // Añade aquí todas las que necesites
+};
